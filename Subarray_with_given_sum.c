ee93f8d538453f78c00a9e116e4d5ce5cbe673f8
LAVIN TOM K ABRAHAM <53207510+Lavin-tom@users.noreply.github.com>
2023-09-18 21:25:57 +0530

    Updated first code

--- Subarray_with_given_sum.c
+++ Subarray_with_given_sum.c
@@ -1,11 +1,19 @@
 /*
-
 Given an unsorted array A of size N that contains only positive integers, 
 find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
 In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
 
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+
 */
-//solution 1
+
+//solution 
 #include <stdio.h>
 #include <stdlib.h>
 void get_array(int[],int);
