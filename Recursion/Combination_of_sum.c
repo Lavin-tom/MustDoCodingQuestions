7fd3134e81b4c1e51499222e906caefb1a028b4a
LAVIN TOM K ABRAHAM <53207510+Lavin-tom@users.noreply.github.com>
2023-10-18 21:18:54 +0530

    Update Combination_of_sum.c

--- Recursion/Combination_of_sum.c
+++ Recursion/Combination_of_sum.c
@@ -14,33 +14,49 @@ Explanation:
 
 */
 //solution
-//not completed
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-void get_array(int arr[],int n);
-void find_pair(int arr[],int n);
-int main()
-{
-    int n,k;
-    printf("enter N and K\n");
-    scanf("%d %d",&n,&k);
-    int *arr = (int*)malloc(n*sizeof(int));
-    get_array(arr,n);
-    find_pair(arr,n);
+
+void find_pair(int arr[], int n, int k, int combination[], int len, int index) {
+    if (k == 0) {
+        // Print the combination
+        printf("{ ");
+        for (int i = 0; i < len; i++) {
+            printf("%d ", combination[i]);
+        }
+        printf("}\n");
+        return;
+    }
+
+    if (k < 0 || index >= n) {
+        return;
+    }
+
+    // Include the current element in the combination
+    combination[len] = arr[index];
+    find_pair(arr, n, k - arr[index], combination, len + 1, index + 1);
+
+    // Exclude the current element from the combination
+    find_pair(arr, n, k, combination, len, index + 1);
+}
+
+void find_combinations(int arr[], int n, int k) {
+    int *combination = (int *)malloc(n * sizeof(int));
+    find_pair(arr, n, k, combination, 0, 0);
+    free(combination);
+}
+
+int main() {
+    int n, k;
+    printf("Enter N and K: ");
+    scanf("%d %d", &n, &k);
+    int *arr = (int *)malloc(n * sizeof(int));
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    find_combinations(arr, n, k);
     free(arr);
     return 0;
-}
-void get_array(int arr[],int n)
-{
-    printf("enter array elements\n");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-}
-void find_pair(int arr[],int n)
-{
-    //not yet implemented
 }
