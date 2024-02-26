dc57b3c027cbd32ed4a9fbcfd58d5c83876e5d9f
Lavin Tom K Abraham <53207510+Lavin-tom@users.noreply.github.com>
2024-02-26 21:43:31 +0530

    Create Jumping_Caterpillars.c

---
+++ Array/Jumping_Caterpillars.c
@@ -0,0 +1,47 @@
+//not completed
+
+#include<stdio.h>
+#include<stdlib.h>
+void getArray(int[]);
+void setArray(int[]);
+void setTree(int[]);
+void findLeave(int []);
+int k;
+int main()
+{
+	int n;
+    printf("enter n and k\n");
+    scanf("%d %d",&n,&k);
+    int *array = (int*)malloc(k*sizeof(int));
+    int *tree = (int*)malloc(n*sizeof(int));
+    setArray(array);
+    setTree(array);
+    findLeave(array);
+    free(array);
+}
+void getArray(int array[])
+{
+    for(int i=0;i<size;i++)
+    {
+        printf("%d ",array[i]);
+    }
+}
+void setArray(int array[])
+{
+    printf("enter array elements\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+}
+void findLeave(int array[])
+{
+	for(int i=0;i<n;i++)
+	{
+		int k = array[i];
+		for(int j=0;j<n;j+=k)
+		{
+			
+		}
+	}
+}
