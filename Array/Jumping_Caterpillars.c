cd7c2154cbc92a3ac8cd9de6f99567eacadcb4b3
Lavin Tom K Abraham <53207510+Lavin-tom@users.noreply.github.com>
2024-03-03 21:39:16 +0530

    Update Jumping_Caterpillars.c

--- Array/Jumping_Caterpillars.c
+++ Array/Jumping_Caterpillars.c
@@ -1,3 +1,27 @@
+//Given N leaves numbered from 1 to N . A caterpillar at leaf 1, 
+//jumps from leaf to leaf in multiples of Aj (Aj, 2Aj, 3Aj).
+//j is specific to the caterpillar. Whenever a caterpillar reaches a leaf, 
+//it eats it a little bit.. You have to find out how many leaves, from 1 to N, 
+//are left uneaten after all K caterpillars have reached the end. 
+//Each caterpillar has its own jump factor denoted by Aj, and each caterpillar starts at leaf number 1.
+
+
+//example
+/*
+
+Input:
+N=10 K=3
+arr[] = {2, 3, 5} 
+Output: 2
+Explanation: The leaves eaten by the first 
+caterpillar are (2, 4, 6, 8, 10). The leaves 
+eaten by the second caterpilllar are (3, 6, 9).
+The leaves eaten by the third caterpilllar 
+are (5, 10). Ultimately, the uneaten leaves are 
+1, 7 and their number is 2.
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 void setCaterpiller(int[]);
@@ -18,7 +42,7 @@ int main()
 }
 void setLeaves(int tree[])
 {
-   for(int i=0;i<k;i++)
+   for(int i=0;i<n;i++)
     tree[i]=i+1;
 }
 void setCaterpiller(int caterpiller[])
@@ -31,17 +55,19 @@ void setCaterpiller(int caterpiller[])
 }
 void findLeave(int caterpiller[],int tree[])
 {
-	for(int i=0;i<n;i++)
+    int count=0;
+	for(int i=0;i<k;i++)
 	{
-		int k = caterpiller[i];
-		for(int j=0;j<n;j+=k)
+		int m = caterpiller[i];
+		for(int j=0;j<n;j+=m)
 		{
-			tree[j]=0;
+			tree[j]=0;      //eaten leaves are marked as '0'
 		}
 	}
-  for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)    //finding leaves without '0'
 	  {
 		  if(tree[i]!=0)
-			  printf("%d",tree[i]);
+			  count++;
 	  }
+	  printf("%d\n",count);
 }
